621e6969031661fc26e779a3578cccfc07111806
abulhussian12 <abulhussian1229@gmail.com>
2024-10-06 16:14:52 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors:{
+        darkPurple:'#301534',
+        pink:'#AD28EB',
+        palePurple:'#8B6990',
+        lightPink:'#F8EEFF',
+        white:'#FFFFFF'
+      }
+    },
+  },
+  plugins: [],
+}
